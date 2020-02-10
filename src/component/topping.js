c718438b36d27b07b31df15238e06dcab7cd5339
Zengwei Xu <xuzengwei06@gmail.com>
2020-02-11 10:34:23 +1100

    init done

---
+++ src/component/topping.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Topping extends React.Component {
+    render() {
+        return (
+            <div>
+                Hello World!
+            </div>
+        )
+    }
+}
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Topping from './component/topping'
+
+ReactDOM.render(<Topping />, document.getElementById('app'));
